a66807fb7199b476e2cdb008292905acfe5a7c18
Moldoteck <cpadureac@gmail.com>
2021-07-03 21:29:56 +0300

    tmp

--- src/commands/voicespeak.ts
+++ src/commands/voicespeak.ts
@@ -188,7 +188,7 @@ export function setupSpeaker(bot: Telegraf<Context>) {
 
       readertmp.append(audio)
 
-      var command = ffmpeg(readertmp).output(writer)
+      var command = ffmpeg(readertmp).audioCodec('libmp3lame').output(writer)
       command.run()
       // var command = ffmpeg(readertmp)
       // let soxed = readertmp.pipe(sox({ input: { type: 'mp3' }, output: { type: 'opus' } }))
