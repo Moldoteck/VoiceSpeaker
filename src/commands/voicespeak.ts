3d88e5f505ad36e3db2ec1e57edd67da36712525
Moldoteck <cpadureac@gmail.com>
2021-07-03 19:42:08 +0300

    add ogg conversion

--- src/commands/voicespeak.ts
+++ src/commands/voicespeak.ts
@@ -189,7 +189,7 @@ export function setupSpeaker(bot: Telegraf<Context>) {
       let reader = new streams.ReadableStream()
       let writer = new streams.WritableStream()
       command.output(writer)
-        .outputFileType('ogg');
+        // .outputFileType('ogg');
 
       // // console.log(writer.toBuffer())
       reader.append(writer.toBuffer())
